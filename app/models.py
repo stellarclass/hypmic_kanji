08307a4cc93a245eb515f0b8e5d528e68ad90252
stellarclass <iman.lau@gmail.com>
2021-07-22 22:44:08 -0400

    i don't know how models work but i think it's better now maybe

--- app/models.py
+++ app/models.py
@@ -3,9 +3,6 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
 
-from datetime import timedelta
-import ebisu
-
 class commonLearning(models.Model):
     #user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
     have_learned = models.BinaryField(default=False)
@@ -13,50 +10,67 @@ class commonLearning(models.Model):
     item = models.TextField()
     date_learned = models.DateTimeField(blank=True, null=True)
     date_last_reviewed = models.DateTimeField(blank=True, null=True)
-    recall_prob = models.FloatField()
+    recall_prob = models.FloatField(blank=True, null=True)
+    alpha = models.FloatField(default = 4.0)
+    beta = models.FloatField(default = 4.0)
+    half_life = models.FloatField(default = 24.0)
+    user_notes = models.TextField(blank=True, null=True)
 
     class Meta:
         abstract = True
 
-    def learn(self):
-        self.date_learned = timezone.now()
-        self.date_last_reviewed = timezone.now()
-        self.save()
-
-    def review(self, bayes_model, quiz_result):
-        self.recall_prob = ebisu.updateRecall([bayes_model.alpha, bayes_model.beta, bayes_model.half_life],
-                                              quiz_result, 1,
-                                              (timezone.now() - self.date_last_reviewed)/timedelta()hours=1)
-        self.date_last_reviewed = timezone.now()
-        self.save()
-
     def __str__(self):
-        return self.item_learned
+        return self.item
 
 class kanji(commonLearning):
+    pass
 
 class vocab(commonLearning):
     linked_kanji = models.ManyToManyField(kanji)
 
 class meanings(models.Model):
-    item = models.ForeignKey(commonLearning, on_delete=models.DO_NOTHING)
+    kanji = models.ForeignKey(kanji, on_delete=models.DO_NOTHING)
+    vocab = models.ForeignKey(vocab, on_delete=models.DO_NOTHING)
     meanings = models.TextField()
+    user_synonyms = models.TextField(blank=True, null=True)
+
+    @property
+    def type_of_item(self):
+        return self.kanji or self.vocab
+
+    @type_of_item.setter
+    def type_of_item(self, obj):
+        if type(obj) == kanji:
+            self.kanji = obj
+            self.vocab = None
+        elif type(obj) == vocab:
+            self.vocab = obj
+            self.kanji = None
+        else:
+            raise ValueError("obj parameter must be an object of Kanji or Vocab class")
 
     def __str__(self):
-        return 'Meaning(s) for %s are %s' % (self.item, self.meanings)
+        return 'Meaning(s) are %s' % (self.meanings)
 
 class readings(models.Model):
-    item = models.ForeignKey(commonLearning, on_delete=models.DO_NOTHING)
+    kanji = models.ForeignKey(kanji, on_delete=models.DO_NOTHING)
+    vocab = models.ForeignKey(vocab, on_delete=models.DO_NOTHING)
     readings = models.TextField()
 
-    def __str__(self):
-        return 'Reading(s) for %s are %s' % (self.item, self.readings)
+    @property
+    def type_of_item(self):
+        return self.kanji or self.vocab
 
-class bayes_model(models.Model):
-    item = models.OneToOneField(commonLearning, on_delete=models.CASCADE)
-    alpha = models.FloatField(default = 4.0)
-    beta = models.FloatField(default = 4.0)
-    half_life = models.FloatField(default = 24.0)
+    @type_of_item.setter
+    def type_of_item(self, obj):
+        if type(obj) == kanji:
+            self.kanji = obj
+            self.vocab = None
+        elif type(obj) == vocab:
+            self.vocab = obj
+            self.kanji = None
+        else:
+            raise ValueError("obj parameter must be an object of Kanji or Vocab class")
 
     def __str__(self):
-        return 'Current model for %s: [%s, %s, %s]' % (self.item, self.alpha, self.beta, self.half_life)
+        return 'Reading(s) are %s' % (self.readings)
